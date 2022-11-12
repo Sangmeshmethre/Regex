a04e3b06a97acdf5c8c58fe21d87e347581f687d
Sangmesh Methre <SangmeshMetre>
2022-11-13 03:48:53 +0530

    Regex At Least One Numaric Number

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserregistarUC7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to REGEX User Registration Problem");
+        Start:
+            Console.WriteLine("Select Options Please :\n" +
+                 "1)Enter Password Rule No. 3\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    UserRegistration user6 = new UserRegistration();
+                    Console.WriteLine(":::::::::::::::::::::::::::::;::");
+                    user6.ValidPasswordRule3();
+                    break;
+
+
+
+                default:
+                    Console.WriteLine("Choose Proper options");
+                    break;
+            }
+            goto Start;
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserregistarUC7
+{
+    public class UserRegistration
+    {
+        public void ValidPasswordRule3()
+        {
+            Console.WriteLine("Please Enter Password Rule 3 :");
+            string data = Console.ReadLine();
+
+            string pattern = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$";
+
+            //creating object of the Regex superclass
+
+            Regex regobj = new Regex(pattern);
+
+            if (regobj.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Password Rule 3 is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Password Rule 3 Invalid");
+                Console.ResetColor();
+            }
+        }
+    }
+}
