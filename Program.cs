76845791a18d25016b66326c831e979dc80d5c36
Sangmesh Methre <SangmeshMetre>
2022-11-13 03:27:52 +0530

    Regex Last Name

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistarUC2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to REGEX User Registration Problem");
+
+            Console.WriteLine("Select Options Please :\n" +
+                "1)Enter First Name\n" +
+                "2)Enter Last Name\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    UserRegistration user = new UserRegistration();
+                    Console.WriteLine(":::::::::::::::::::::::::::::::");
+                    user.ValidFirstName();
+                    break;
+
+                case 2:
+
+                    UserRegistration user1 = new UserRegistration();
+                    Console.WriteLine(":::::::::::::::::::::::::::::::");
+                    user1.ValidLastName();
+                    break;
+
+                default:
+                    Console.WriteLine("Choose Proper options");
+                    break;
+            }
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistarUC2
+{
+    public class UserRegistration
+    {
+        //UC1
+        public void ValidFirstName()
+        {
+            Console.WriteLine("Please Enter First Name :");
+            string data = Console.ReadLine();
+
+            string pattern = "^[A-Z][a-z]{2,10}$";
+
+            //creating object of the Regex superclass
+
+            Regex regobj = new Regex(pattern);
+
+            if (regobj.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("First Name is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("First Name is Invalid");
+                Console.ResetColor();
+            }
+        }
+
+        //UC2
+        public void ValidLastName()
+        {
+            Console.WriteLine("Please Enter Last Name :");
+            string data = Console.ReadLine();
+
+            string pattern = "^[A-Z][a-z]{2,12}$";
+
+            //creating object of the Regex superclass
+
+            Regex regobj = new Regex(pattern);
+
+            if (regobj.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Last Name is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Last Name is Invalid");
+                Console.ResetColor();
+            }
+        }
+    }
+}
