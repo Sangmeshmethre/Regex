e867a383c96c05ccc515d10d341e587de49cd130
Sangmesh Methre <SangmeshMetre>
2022-11-13 03:24:10 +0530

    Regex Valide Name

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace REGEX_UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to REGEX User Registration Problem");
+
+            Console.WriteLine("Select Options Please :\n" +
+                "1)Enter First Name\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    UserRegistration user = new UserRegistration();
+                    user.ValidFistName();
+                    break;
+
+                default:
+                    Console.WriteLine("Choose Proper options");
+                    break;
+            }
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace REGEX_UserRegistration
+{
+    public class UserRegistration
+    {
+        public void ValidFistName()
+        {
+            Console.WriteLine("Please Enter First Name :");
+            string data = Console.ReadLine();
+            string pattern = "^[A-Z][a-z]{2,10}$";
+            //Creating Object of the Regex SuperClass
+            Regex regobj = new Regex(pattern);
+            if(regobj.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("First Name is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("First Name Is Invalide");
+                Console.ResetColor();
+            }
+        }
+    }
+}
