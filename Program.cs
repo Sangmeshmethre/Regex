a4cac63bb2457005ab7c59741d7b8b8fdf8e0117
Sangmesh Methre <SangmeshMetre>
2022-11-13 03:31:06 +0530

    Regex Valide Email

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegisterUC3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to REGEX User Registration Problem");
+        Start:
+            Console.WriteLine("Select Options Please :\n" +
+                "1)Enter First Name\n" +
+                "2)Enter Last Name\n" +
+                "3)Enter E-Mail Address\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    UserRegistration user = new UserRegistration();
+                    Console.WriteLine(":::::::::::::::::::::::::::::::");
+                    user.ValidFirstName();
+                    break;
+
+                case 2:
+
+                    UserRegistration user1 = new UserRegistration();
+                    Console.WriteLine(":::::::::::::::::::::::::::::::");
+                    user1.ValidLastName();
+                    break;
+
+                case 3:
+                    UserRegistration user2 = new UserRegistration();
+                    Console.WriteLine(":::::::::::::::::::::::::::::::");
+                    user2.ValidEmail();
+                    break;
+
+
+                default:
+                    Console.WriteLine("Choose Proper options");
+                    break;
+            }
+            goto Start;
+        }
+    }
+}
